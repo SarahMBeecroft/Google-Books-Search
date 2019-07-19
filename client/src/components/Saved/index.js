320e10f68a142361b7a4fdb0eee29a87f1178303
Sarah B <smblair87@gmail.com>
2019-07-18 21:58:59 -0700

    Save book functionality is working

---
+++ client/src/components/Saved/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './style.css';
+import { Row, Col } from '../Grid';
+
+const SavedResult = props => {
+  return (props.savedBooks.length === 0) ? (
+    <div className='card'>
+      <div className='card-body player'>
+        <div className='article'>
+          <h3>Saved Books:</h3>
+        </div>
+      </div>
+    </div>
+  ) : (
+      <div className='card-body player'>
+        <div className='article'>
+          <h3>Saved Books:</h3>
+          {props.savedBooks.map(savedBook => {
+            return (
+              <li className='saved-list list-group-item' key={savedBook._id}>
+                <Row className='SearchResult' id={savedBook.title + 'Card'} key={savedBook._id}>
+                  <Col size='2' className='bookImage'>
+                    <img src={savedBook.image} alt={savedBook.title} />
+                  </Col>
+                  <Col size='1' className='emptyCol' />
+                  <Col size='9' className='bookInfo'>
+                    <Row>
+                      <h2 className='bookTitle'>{savedBook.title}</h2>
+                    </Row>
+                    <Row>
+                      {savedBook.authors.map(author => {
+                        return (
+                          <h3 className='bookAuthor'>{author}, </h3>
+                        )
+                      })}
+
+                    </Row>
+                    <Row>
+                      <p className='bookDescription'>{savedBook.description}</p>
+                    </Row>
+                  </Col>
+                </Row>
+                <br></br>
+                <Row className='buttonDiv'>
+                  <button className='deleteBtn btn btn-danger' id={savedBook._id} onClick={() => props.handleDeleteButton(savedBook._id)}>
+                    Delete Book
+                                    </button>
+                  <a href={savedBook.link} target='_blank' rel='noopener noreferrer'>
+                    <button className='viewBook btn btn-success'>
+                      View Book
+                                        </button>
+                  </a>
+                </Row>
+              </li>
+            );
+          })}
+        </div>
+      </div>
+    )
+}
+
+export default SavedResult
---
+++ client/src/pages/Saved/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Container } from '../../components/Grid';
+import Jumbotron from '../../components/Jumbotron';
+import API from '../../utils/API';
+import Saved from '../../components/Saved';
+
+class SaveBook extends Component {
+  state = {
+    savedBooks: []
+  };
+  // When this component mounts, grab the book with the _id of this.props.match.params.id
+  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
+  componentDidMount() {
+    API.getBooks()
+      // .then(res => console.log(res.data))
+      .then(res => this.setState({ savedBooks: res.data }))
+      .catch(err => console.log(err))
+  }
+
+  handleDeleteButton = id => {
+    API.deleteBook(id)
+      .then(res => this.componentDidMount())
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Jumbotron />
+        <Container>
+          <Saved
+            savedBooks={this.state.savedBooks}
+            handleDeleteButton={this.handleDeleteButton}
+          />
+        </Container>
+      </Container>
+    );
+  }
+}
+
+export default SaveBook;
